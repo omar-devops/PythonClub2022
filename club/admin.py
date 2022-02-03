0a5d20c2102dfe479f79aab4f2de6ef27a4753b9
Omar <omkaydin@gmail.com>
2022-02-02 19:59:13 -0800

    model commit

--- club/admin.py
+++ club/admin.py
@@ -1,3 +1,21 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from .models import Meeting
+# # , Minutes, Resource, Event
 
+# # Register your models here.
+# admin.site.register('Meeting')
+# # # admin.site.register('Minutes')
+# # # admin.site.register('Resource')
+# # # admin.site.register('Event')
+
+from django.contrib import admin
+from .models import Club, Meeting, Minutes, Resource, Event
 # Register your models here.
+# Necessary if they are to appear in the admin
+admin.site.register(Club)
+admin.site.register(Meeting)
+admin.site.register(Minutes)
+admin.site.register(Resource)
+admin.site.register(Event)
+
+
--- club/models.py
+++ club/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from  django.contrib.auth.models import User
 
-# Create your models here.
+class Club(models.Model):
+    Clubname = models.CharField(max_length=255)
+    clubdescription=models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.Clubname
+    
+    class Meta:
+        db_table='Club'
+
+class Meeting(models.Model):
+    meetingTitle = models.CharField(max_length=255)
+    meetingDate = models.DateField()
+    meetingLocation = models.TextField()
+    meetingTime=models.DurationField(null=True)
+    meetingAgenda = models.TextField()
+
+    def __str__(self):
+        return self.meetingTitle
+    
+    class Meta:
+        db_table='Meeting'
+    
+class Minutes(models.Model):
+    # minutesmeeting=models.ForeignKey(Meeting, on_delete=models.CASCADE)
+    meetingId=models.ForeignKey(Meeting, on_delete=models.CASCADE)
+    attendance = models.ManyToManyField(User)
+    text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.text
+    
+    class Meta:
+        db_table='Minutes'
+
+class Resource(models.Model):
+    resourceName=models.CharField(max_length=255)
+    meetingMinutes=models.ForeignKey(Minutes, on_delete=models.CASCADE)
+    resourceType = models.CharField(max_length=255)
+    dateEntered = models.DateTimeField()
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    url = models.URLField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.resourceName
+
+    class Meta:
+        db_table='Resource'
+
+class Event(models.Model):
+    eventTitle=models.CharField(max_length=255)
+    eventResource=models.ForeignKey(Resource, on_delete=models.CASCADE)
+    eventLocation = models.CharField(max_length=255)
+    eventDate = models.DateTimeField()
+    eventUserId = models.ForeignKey(User, on_delete=models.CASCADE)
+    url = models.URLField()
+    eventDesc = models.TextField()
+
+    def __str__(self):
+        return self.eventTitle
+
+    class Meta:
+        db_table='Event'
