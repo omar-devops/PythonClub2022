2e3c112a3147d4a887f95edf3304c02b8a7bbc8f
Omar <omkaydin@gmail.com>
2022-02-07 23:05:06 -0800

    Add members

--- club/views.py
+++ club/views.py
@@ -1,5 +1,12 @@
+from collections import UserList
 from django.shortcuts import render
+from .models import Meeting, Event, Resource, Minutes
 
 # Create your views here.
 def index(request):
     return render(request, 'club/index.html')
+
+def members(request):
+    user_list = Event.objects.all()
+    return render(request, 'club/members.html', {'user_list': user_list})
+
