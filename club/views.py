c82d925d3aa5111c56fb7ec4be2d2d000017eb96
Omar <omkaydin@gmail.com>
2022-02-08 21:31:14 -0800

    Assignment 4

--- club/views.py
+++ club/views.py
@@ -1,12 +1,12 @@
 from collections import UserList
 from django.shortcuts import render
-from .models import Meeting, Event, Resource, Minutes
+from .models import Club, Meeting, Event, Resource, Minutes
 
 # Create your views here.
 def index(request):
     return render(request, 'club/index.html')
 
 def members(request):
-    user_list = Event.objects.all()
+    user_list = Resource.objects.all()
     return render(request, 'club/members.html', {'user_list': user_list})
 
